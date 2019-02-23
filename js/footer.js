9abd21312ff92b4a76d4fba0e44e6cb25e9e6609
ws10204 <2942504702@qq.com>
2019-02-23 20:05:52 +0800

    2019/2/23

---
+++ js/footer.js
@@ -0,0 +1,5 @@
+document.writeln("<footer>");
+document.writeln("        <p>版权所有&nbsp;&nbsp;南都公益基金会京CP备12048184号&nbsp;&nbsp;&nbsp;地址:北京朝阳区朝外大街甲6号万通中心C座1505室&nbsp;&nbsp;&nbsp;&nbsp;邮编:100020");
+document.writeln("        </p>");
+document.writeln("        <p>电话:010-51656856&nbsp;&nbsp;&nbsp;&nbsp;网站管理及技术支持:深圳互联心有限公司</p>");
+document.writeln("    </footer>");
